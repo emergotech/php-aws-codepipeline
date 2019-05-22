8702a5aaf9eb3f2402c6fbe5dee35d34f6bae553
Khalid Bin Sattar <42968874+khalids226@users.noreply.github.com>
2019-05-23 00:54:27 +0600

    second commit

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 
 This is a Test for Emergotech tested by Khalid with Integrate the Production Server.+ 
 <?php
-echo 'Hello, World from Khalid to test the Cloud9!'; ?>
+echo 'Hello, World from Khalid to test the Cloud9 to test with Marwan!!'; ?>
